7d2eb03cc09c55eb9ec6a612b48a4034b0c3dba2
KevinPolanco <polancockp@gmail.com>
2023-04-04 19:31:25 -0400

    1,14*: anecdotes, paso 3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,6 +1,33 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const MaxVotes = ({ anecdotes, copyVote }) => {
+  const votes = [];
+  let mostVotedAnecdote = 0;
+  
+  for (const prop in copyVote) {
+    votes.push(copyVote[prop]);
+  }
+  
+  votes.sort((a, b) => b - a);
+
+  for (const prop in copyVote) {
+    if (copyVote[prop] === votes[0]) {
+      mostVotedAnecdote = prop;
+    }
+  }
+
+  if (votes[0] > 0) {
+    return (
+      <>
+        <h2>Anecdote with most votes</h2>
+        <strong>{anecdotes[mostVotedAnecdote]}</strong>
+        <p>has {votes[0]} votes</p>
+      </>
+    );
+  }
+};
+
 const App = (props) => {
   const [selected, setSelected] = useState(0);
   const lengthAnecdotes = props.anecdotes.length;
@@ -20,7 +47,8 @@ const App = (props) => {
 
   return (
     <div>
-      {props.anecdotes[selected]}
+      <h2>Anecdote of the day</h2>
+      <strong>{props.anecdotes[selected]}</strong>
       <p>has {vote[selected]} votes</p>
       <button onClick={() => setSelected(getRandomInt(lengthAnecdotes))}>
         next anecdote
@@ -34,6 +62,7 @@ const App = (props) => {
       >
         vote
       </button>
+      <MaxVotes anecdotes={props.anecdotes} copyVote={copyVote} />
     </div>
   );
 };
