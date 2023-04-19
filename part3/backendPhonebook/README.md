bc2178e184b56f3f3e717609738c3d3c6e6f2a96
KevinPolanco <polancockp@gmail.com>
2023-04-19 13:47:05 -0400

    3.10: backendPhonebook , steap 10

---
+++ part3/backendPhonebook/README.md
@@ -0,0 +1,1 @@
+https://backend-phonebook.herokuapp.com/api/persons/
--- part3/backendPhonebook/index.js
+++ part3/backendPhonebook/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 var morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json());
+app.use(cors())
 
 app.use(morgan('tiny'))
 morgan.token('body', (request) => JSON.stringify(request.body))
@@ -109,7 +111,7 @@ app.post("/api/persons", morgan(':body'), (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
