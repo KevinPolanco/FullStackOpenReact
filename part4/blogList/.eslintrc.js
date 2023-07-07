064cfdd91bfefd7e04345234c4a3a26ff6a0efff
KevinPolanco <polancockp@gmail.com>
2023-07-07 19:59:37 -0400

    4.2 Blog list, step 2

---
+++ part4/blogList/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": {
+    "indent": [
+      "warn",
+      2
+    ],
+    "linebreak-style": [
+      "warn",
+      "windows"
+    ],
+    "quotes": [
+      "warn",
+      "double"
+    ],
+    "semi": [
+      "warn",
+      "always"
+    ]
+  }
+};
