08883ed1b80f605daba2642b4f838c36443f0083
KevinPolanco <polancockp@gmail.com>
2023-07-07 22:41:37 -0400

    4.4: helper functions and unit tests, step 2

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = {
-  dummy
+const totalLikes = (listBlogs) => {
+  let totalLikes = listBlogs.reduce((sum, item) => {
+    return sum + item.likes ; 
+  },0);
+  return totalLikes;
 };
+
+module.exports = {
+  dummy,
+  totalLikes
+};
+
