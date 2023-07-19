c93e8d5fca72321742ad02397552a91799a6aa3b
KevinPolanco <polancockp@gmail.com>
2023-07-19 00:46:32 -0400

    4.8: Blog list tests, step 1

---
+++ part4/blogList/tests/blog_api.test.js
@@ -0,0 +1,32 @@
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+const helper = require("./test_helper");
+const app = require("../app");
+const Blog = require("../models/blog");
+
+mongoose.set("bufferTimeoutMS", 30000);
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
+
+const api = supertest(app);
+
+test("all blogs are returned as json", async () => {
+  const response = await api
+    .get("/api/blogs")
+    .expect("Content-Type", /application\/json/);
+  
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
+
+
+afterAll(() => {
+  mongoose.connection.close();
+});
--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -2,15 +2,22 @@ const dummy = (blogs) => {
   return 1;
 };
 
-const totalLikes = (listBlogs) => {
-  let totalLikes = listBlogs.reduce((sum, item) => {
+const totalLikes = (arrayBlogs) => {
+  let totalLikes = arrayBlogs.reduce((sum, item) => {
     return sum + item.likes ; 
   },0);
   return totalLikes;
 };
 
+const favoriteBlog = (arrayBlogs) => {
+  arrayBlogs.sort((a, b) => b.likes - a.likes);
+
+  return arrayBlogs[0];
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
 
