2c4eb25c01c8b1034707817b5cf067e6c7c6fdb5
KevinPolanco <polancockp@gmail.com>
2023-07-24 20:24:04 -0400

    4.19: bloglist expansion, step 7

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -1,5 +1,6 @@
 const config = require("./utils/config");
 const express = require("express");
+require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
@@ -8,6 +9,7 @@ const loginRouter = require("./controllers/login");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+
 
 logger.info("connecting to", config.MONGODB_URI);
 
