75cc7e4af38f3b99c1822530559ec1f4ef5d3606
KevinPolanco <polancockp@gmail.com>
2023-07-25 22:21:27 -0400

    4.20*: blog list expansion, step 8

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -10,7 +10,6 @@ const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
-
 logger.info("connecting to", config.MONGODB_URI);
 
 mongoose.connect(config.MONGODB_URI)
@@ -25,6 +24,7 @@ app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
 app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -23,17 +23,10 @@ blogsRouter.get("/:id", async (request, response) => {
   }
 });
 
-const getTokenFrom = request => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
-  const token = await getTokenFrom(request);
+  const token = request.token;
+
   const decodedToken = jwt.verify(token, process.env.SECRET);
   if (!token || !decodedToken.id) {
     return response.status(401).json({ error: "token missing or invalid"});
