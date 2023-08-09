974d5abed9ce24b1f07e88b915e373568663bc76
KevinPolanco <polancockp@gmail.com>
2023-08-09 12:02:39 -0400

    5.15: Blog list tests, step 3

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -50,5 +50,38 @@ describe('<Blog />', () => {
     expect(div).toHaveTextContent('https://overreacted.io/npm-audit-broken-by-design/')
     expect(div).toHaveTextContent('4')
   })
-
+  
+  test('When the button is clicked, the event handler is called the corresponding times', async () => {
+    const blog = [
+      {
+        author: 'Dan Abramov',
+        title: 'npm audit: Broken by Design',
+        url: 'https://overreacted.io/npm-audit-broken-by-design/',
+        likes:'4',
+        user: [
+          {
+            'name': 'Matti Lukkainen',
+          }
+        ]
+      }
+    ]
+  
+    const user = {
+      'name': 'Matti Lukkainen',
+    }
+  
+    const mockHandler = jest.fn()
+  
+    render(<Blog blogs={blog} user={user} updateLike={mockHandler}/>)
+  
+    const viewButton = screen.getByText('view')
+    await userEvent.click(viewButton)
+  
+    const likeButton = screen.getByText('like')
+    await userEvent.click(likeButton)
+    await userEvent.click(likeButton)
+  
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  })
+  
 })
