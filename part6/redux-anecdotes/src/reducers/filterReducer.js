66a290cd0d9e94c64514f0cc595ff9ba5ca0b89f
KevinPolanco <polancockp@gmail.com>
2023-08-18 12:57:28 -0400

    6.10 Better anecdotes, step 8

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,18 +1,15 @@
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
-      return action.payload
-    default:
-      return state
-  }
-}
+import { createSlice } from '@reduxjs/toolkit'
 
-export const filterChange = filter => {
-  const newFilter = filter.toLocaleLowerCase()
-  return {
-    type: 'SET_FILTER',
-    payload: newFilter,
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    filterChange(state, action) {
+      const newFilter = action.payload.toLocaleLowerCase()
+      return newFilter
+    }
   }
-}
+})
 
-export default filterReducer
+export const { filterChange }  = filterSlice.actions
+export default filterSlice.reducer
